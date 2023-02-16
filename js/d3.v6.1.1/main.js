65b69ce35a14543f4aae37889dfd8899c02a31be
Jack Krolik <jackkrolik@Jacks-MacBook-Pro.local>
2023-02-16 18:48:37 -0500

    push for scatter plot

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -1,13 +1,9 @@
 
 
-const FRAME_HEIGHT = 200;
+const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500;
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
-
-// with scale function
-const SCATTER_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const SCATTER_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 
 const FRAME1 = d3.select('.scatter')
                     .append("svg")
@@ -15,8 +11,28 @@ const FRAME1 = d3.select('.scatter')
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame")
 
+
+// with scale function
+const SCATTER_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const SCATTER_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+
 // Reading from file
 d3.csv("data/scatter-data.csv").then((data) => {
+
+    const MAX_X = 1 + d3.max(data, (d) => 
+                                {return parseInt(d.x)})
+    
+    const MAX_Y = 1 + d3.max(data, (d) => 
+                                {return parseInt(d.y)})
+    
+    const X_SCALE = d3.scaleLinear()
+                            .domain([0, (MAX_X)])
+                            .range([0, SCATTER_WIDTH]);
+    
+
+    const Y_SCALE = d3.scaleLinear()
+                        .domain([0, (MAX_Y)])
+                        .range([SCATTER_HEIGHT, 0]);
 
 
     // plot
@@ -24,9 +40,21 @@ d3.csv("data/scatter-data.csv").then((data) => {
             .data(data)
             .enter()
             .append("circle")
-                .attr("cx", (d) => {return parseInt(d.x) + MARGINS.left})
-                .attr("cy", (d) => {return parseInt(d.y) + MARGINS.top})
+                .attr("cx", (d) => {return (X_SCALE(parseInt(d.x)) + MARGINS.left)})
+                .attr("cy", (d) => {return (MARGINS.top + (Y_SCALE(parseInt(d.y))))})
                 .attr("r", 10);
+
+    FRAME1.append("g")
+        .attr("transform", "translate(" + MARGINS.top + "," + 
+        (SCATTER_HEIGHT + MARGINS.top) + ")")
+        .call(d3.axisBottom(X_SCALE).ticks(10))
+            .attr("font-size", "15px")
+    
+    FRAME1.append("g")
+        .attr("transform", "translate(" + 
+        (MARGINS.left) + "," + (MARGINS.top) + ")")
+        .call(d3.axisLeft(Y_SCALE).ticks(10))
+            .attr("font-size", "15px")
 
 
 });
