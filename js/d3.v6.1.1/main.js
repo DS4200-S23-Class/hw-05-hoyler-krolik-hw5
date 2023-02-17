c947bd92474ebbdf9ece3a50ef2c079daf3c2210
Jack Krolik <jackkrolik@Jacks-MacBook-Pro.local>
2023-02-17 08:50:25 -0500

    push for hover

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -5,11 +5,11 @@ const FRAME_WIDTH = 500;
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
 
-const FRAME1 = d3.select('.scatter')
+const FRAME1 = d3.select('.chart')
                     .append("svg")
                     .attr("height", FRAME_HEIGHT)
                     .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame")
+                    .attr("class", "frame");
 
 
 // with scale function
@@ -20,10 +20,10 @@ const SCATTER_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
 d3.csv("data/scatter-data.csv").then((data) => {
 
     const MAX_X = 1 + d3.max(data, (d) => 
-                                {return parseInt(d.x)})
+                                {return parseInt(d.x)});
     
     const MAX_Y = 1 + d3.max(data, (d) => 
-                                {return parseInt(d.y)})
+                                {return parseInt(d.y)});
     
     const X_SCALE = d3.scaleLinear()
                             .domain([0, (MAX_X)])
@@ -42,19 +42,43 @@ d3.csv("data/scatter-data.csv").then((data) => {
             .append("circle")
                 .attr("cx", (d) => {return (X_SCALE(parseInt(d.x)) + MARGINS.left)})
                 .attr("cy", (d) => {return (MARGINS.top + (Y_SCALE(parseInt(d.y))))})
-                .attr("r", 10);
+                .attr("r", 10)
+                .attr("class", "scatter-point");
 
     FRAME1.append("g")
         .attr("transform", "translate(" + MARGINS.top + "," + 
         (SCATTER_HEIGHT + MARGINS.top) + ")")
         .call(d3.axisBottom(X_SCALE).ticks(10))
-            .attr("font-size", "15px")
+            .attr("font-size", "15px");
     
     FRAME1.append("g")
         .attr("transform", "translate(" + 
         (MARGINS.left) + "," + (MARGINS.top) + ")")
         .call(d3.axisLeft(Y_SCALE).ticks(10))
-            .attr("font-size", "15px")
+            .attr("font-size", "15px");
 
+    const TOOLTIP = d3.selectAll(".scatter-point")
+    .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+                
+    function handleMouseover(event, d) {
+        TOOLTIP.style("opacity", 1)
+    }
+
+    function handleMousemove(event, d) {
+        TOOLTIP.html('<Hi>')
+                .style("left", (event.pageX + MARGINS.left) + "px")
+                .style("top", (event.pageY - MARGINS.top + "px"))
+    }
+
+    function handleMouseLeave(event, d) {
+        TOOLTIP.style("opacity", 0)
+    }
+
+    FRAME1.selectAll(".scatter-point")
+                .on("mouseover", handleMouseover)
+                .on("mousemove", handleMousemove)
+                .on("mouseleave", handleMouseLeave)
 
 });
